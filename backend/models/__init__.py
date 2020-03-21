8ad2b1dc22f5602372b9ab63b6acf35a7f45599a
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-03-21 11:28:07 +0100

    Add shop column

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -2,3 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+class Shop(db.Model):
+    ''' A shop '''
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.UnicodeText(), nullable=False)
+    address = db.Column(db.UnicodeText(), nullable=False)
+    capacity = db.Column(db.Integer)
+    contact_info = db.Column(db.UnicodeText(), nullable=False)
