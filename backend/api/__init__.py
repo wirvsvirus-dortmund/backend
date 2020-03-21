d4809ac617718308232accb13816537c8d969638
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-03-21 23:02:56 +0100

    Add post method for creating new shops

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, abort, reqparse
 import dateutil.parser
 
 from ..models import db, Shop, CustomerDatapoint
+from ..authentication import login_required
 
 
 api_bp = Blueprint('api_bp', __name__)
@@ -26,30 +27,54 @@ def all_as_dict(query):
     return [as_dict(row) for row in query]
 
 
-def abort_if_not_exists(model, id_):
+def get_or_404(model, id_):
     '''return with 404 if there is no instance of a given model with id `id_`'''
-    if model.query.get(id_) is None:
+    result = model.query.get(id_)
+    if result is None:
         abort(404, message=f'{model.__name__} with id {id_} does not exist')
+    else:
+        return result
 
 
-class Shops(Resource):
+class ShopListAPI(Resource):
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument('name', required=True)
+    parser.add_argument('address', required=True)
+    parser.add_argument('capacity', type=int, required=True)
+    parser.add_argument('contact_info', required=True)
+
     def get(self):
         return dict(status='success', shops=all_as_dict(Shop.query.all()))
 
+    @login_required
+    def post(self):
+        '''Adds a new shop'''
+        args = self.parser.parse_args()
+        new_shop = Shop(**args)
+        db.session.add(new_shop)
+        db.session.commit()
+        return dict(status='success', shop=as_dict(new_shop)), 201
 
-class Customers(Resource):
+
+class ShopAPI(Resource):
+    def get(self, shop_id):
+        return as_dict(get_or_404(Shop, shop_id))
+
+
+class CustomersAPI(Resource):
     parser = reqparse.RequestParser(bundle_errors=True)
     parser.add_argument('timestamp', type=parse_date, required=True)
     parser.add_argument('customers_inside', type=int, required=True)
     parser.add_argument('queue_size', type=int, required=True)
 
     def get(self, shop_id):
-        abort_if_not_exists(Shop, shop_id)
+        get_or_404(Shop, shop_id)
         data = all_as_dict(CustomerDatapoint.query.filter_by(shop_id=shop_id))
         return dict(status='success', shop_id=shop_id, customers=data)
 
     def post(self, shop_id):
-        abort_if_not_exists(Shop, shop_id)
+        '''adds a new data point to the customer timeseries'''
+        get_or_404(Shop, shop_id)
         args = self.parser.parse_args()
         data = CustomerDatapoint(**args, shop_id=shop_id)
         db.session.add(data)
@@ -57,5 +82,6 @@ class Customers(Resource):
         return dict(status='success', data=as_dict(data)), 201
 
 
-api.add_resource(Shops, '/shops')
-api.add_resource(Customers, '/shops/<int:shop_id>/customers')
+api.add_resource(ShopListAPI, '/shops')
+api.add_resource(ShopAPI, '/shops/<int:shop_id>')
+api.add_resource(CustomersAPI, '/shops/<int:shop_id>/customers')
