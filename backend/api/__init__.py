5a267120744dbbcb8dd488886a844e64a0dc1e79
beckstev <beckstev@web.de>
2020-03-21 18:34:53 +0100

    API is now able to querey all shops

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,13 +1,25 @@
 from flask import (jsonify, request, render_template, Blueprint)
 from flask_restful import Api, Resource, url_for
 
+from ..models import db, Shop
+
+
 api_bp = Blueprint('api_bp', __name__)
 api = Api(api_bp)
 
+def as_dict(instance):
+    ''' Transform a model instance into a dict '''
+    return {
+        c.name: getattr(instance, c.name)
+        for c in instance.__table__.columns
+    }
 
-shops = dict()
 class Shops(Resource):
     def get(self):
-        return "Hello World"
+        all_saved_shops = [as_dict(shop) for shop in Shop.query.all()]
+        if all_saved_shops:
+            return jsonify(all_saved_shops)
+        else:
+            return "Currently we haven't stored any shops in our database."
 
 api.add_resource(Shops, '/shops')
